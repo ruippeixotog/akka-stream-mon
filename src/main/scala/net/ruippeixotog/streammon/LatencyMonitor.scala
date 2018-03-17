b0defbfe38710729f0846ec73fb3bb0145c8b9f7
Rui Gonçalves <ruippeixotog@gmail.com>
2018-03-17 19:04:41 +0000

    Add Scaladoc

--- src/main/scala/net/ruippeixotog/streammon/LatencyMonitor.scala
+++ src/main/scala/net/ruippeixotog/streammon/LatencyMonitor.scala
@@ -10,6 +10,21 @@ import akka.stream.stage._
 
 import net.ruippeixotog.streammon.LatencyMonitor.{ Stats, TimerContext }
 
+/**
+ * A graph stage measuring the latency of a given linear segment (`Flow`) of a graph. The graph wraps an user-provided
+ * `Flow`, connecting `in` to its input port and `out` to its output port. A second output port `statsOut` emits
+ * statistics of the time taken by elements to go from `in` to `out`.
+ *
+ * In order for latency measures to be meaningful, the `Flow` being measured is required by this stage to:
+ *   - emit exactly one element for each one that it consumes (i.e. it doesn't aggregate, filter or unfold elements,
+ *     just transforms them);
+ *   - emits elements the in the same order it consumes them.
+ *
+ * `statsOut` emits continuously as demanded by downstream; the connected `Sink` is responsible for throttling demand,
+ * controlling that way the update frequency of the stats (or, equivalently, the size of the buckets they represent).
+ *
+ * @tparam A the type of the elements passing through this stage
+ */
 class LatencyMonitor[A](ctx: TimerContext) extends GraphStage[FanOutShape2[A, A, Stats]] {
 
   val in = Inlet[A]("LatencyMonitor.in")
@@ -50,10 +65,6 @@ class LatencyMonitor[A](ctx: TimerContext) extends GraphStage[FanOutShape2[A, A,
 
 object LatencyMonitor {
 
-  case class Stats(timeElapsed: Long, count: Long, sumLatency: Long) {
-    def avgLatency: Double = sumLatency.toDouble / count
-  }
-
   private class TimerContext {
     private val queue = new ConcurrentLinkedQueue[Long]
 
@@ -61,6 +72,30 @@ object LatencyMonitor {
     def stop(): Long = (System.nanoTime() - queue.poll()) / 1000000
   }
 
+  /**
+   * Aggregate latency metrics of a flow.
+   *
+   * @param timeElapsed the time elapsed between the measurement start and its end, in milliseconds
+   * @param count the number of elements that passed through the flow
+   * @param sumLatency the sum of the latencies of all the elements that passed through the flow, in milliseconds
+   */
+  case class Stats(timeElapsed: Long, count: Long, sumLatency: Long) {
+
+    /**
+     * The average latency of the elements that passed through the flow, in milliseconds
+     */
+    def avgLatency: Double = sumLatency.toDouble / count
+  }
+
+  /**
+   * Creates a `LatencyMonitor` stage.
+   *
+   * @param flow the `Flow` to be measured
+   * @tparam A the input type of `flow`
+   * @tparam B the output type of `flow`
+   * @tparam Mat the materialized value of `flow`
+   * @return a `LatencyMonitor` stage measuring `flow`.
+   */
   def apply[A, B, Mat](flow: Flow[A, B, Mat]): Graph[FanOutShape2[A, B, Stats], Mat] = {
     GraphDSL.create(flow) { implicit b => fl =>
       import GraphDSL.Implicits._
@@ -73,6 +108,19 @@ object LatencyMonitor {
     }
   }
 
+  /**
+   * Creates a `LatencyMonitor` stage with latency stats consumed by a given `Sink`.
+   *
+   * @param flow the `Flow` to be measured
+   * @param statsSink the `Sink` that will consume latency statistics
+   * @param combineMat a function combining the materialized values of `flow` and `statsSink`
+   * @tparam A the input type of `flow`
+   * @tparam B the output type of `flow`
+   * @tparam Mat the materialized value of `flow`
+   * @tparam Mat2 the materialized value of `statsSink`
+   * @tparam Mat3 the materialized value of the returned stage
+   * @return a `Flow` that passes all elements through `flow` and emits latency stats to `statsSink`.
+   */
   def apply[A, B, Mat, Mat2, Mat3](
     flow: Flow[A, B, Mat],
     statsSink: Sink[Stats, Mat2])(combineMat: (Mat, Mat2) => Mat3): Graph[FlowShape[A, B], Mat3] = {
@@ -84,6 +132,17 @@ object LatencyMonitor {
     }
   }
 
+  /**
+   * Creates a `LatencyMonitor` stage with latency stats handled periodically by a callback.
+   *
+   * @param flow the `Flow` to be measured
+   * @param statsInterval the update frequency of the latency stats
+   * @param onStats the function to call when a layency stats bucket is available
+   * @tparam A the input type of `flow`
+   * @tparam B the output type of `flow`
+   * @tparam Mat the materialized value of `flow`
+   * @return a `Flow` that passes all elements through `flow` and calls `onStats` frequently with latency stats.
+   */
   def apply[A, B, Mat](flow: Flow[A, B, Mat], statsInterval: FiniteDuration, onStats: Stats => Unit): Graph[FlowShape[A, B], Mat] =
     apply(flow, Flow.fromGraph(new Pulse[Stats](statsInterval)).to(Sink.foreach(onStats)))(Keep.left)
 }
--- src/main/scala/net/ruippeixotog/streammon/ThroughputMonitor.scala
+++ src/main/scala/net/ruippeixotog/streammon/ThroughputMonitor.scala
@@ -9,6 +9,16 @@ import akka.stream.stage._
 
 import net.ruippeixotog.streammon.ThroughputMonitor.Stats
 
+/**
+ * A graph stage measuring the element throughput at a given point in a graph. The stage emits through `out` all
+ * elements recieved at `in`, while a second output port `statsOut` emits statistics of the number of elements passing
+ * per unit of time at the `in`-`out` edge.
+ *
+ * `statsOut` emits continuously as demanded by downstream; the connected `Sink` is responsible for throttling demand,
+ * controlling that way the update frequency of the stats (or, equivalently, the size of the buckets they represent).
+ *
+ * @tparam A the type of the elements passing through this stage
+ */
 class ThroughputMonitor[A] extends GraphStage[FanOutShape2[A, A, Stats]] {
 
   val in = Inlet[A]("ThroughputMonitor.in")
@@ -47,13 +57,37 @@ class ThroughputMonitor[A] extends GraphStage[FanOutShape2[A, A, Stats]] {
 
 object ThroughputMonitor {
 
+  /**
+   * Aggregate throughput metrics of a stream.
+   *
+   * @param timeElapsed the time elapsed between the measurement start and its end, in milliseconds
+   * @param count the number of elements that passed through the stream
+   */
   case class Stats(timeElapsed: Long, count: Long) {
+
+    /**
+     * The number of elements that passed through the stream per second.
+     */
     def throughput: Double = count.toDouble * 1000 / timeElapsed
   }
 
+  /**
+   * Creates a `ThroughputMonitor` stage.
+   *
+   * @tparam A the type of the elements passing through this stage
+   * @return a `ThroughputMonitor` stage.
+   */
   def apply[A]: Graph[FanOutShape2[A, A, Stats], NotUsed] =
     new ThroughputMonitor[A]
 
+  /**
+   * Creates a `ThroughputMonitor` stage with throughput stats consumed by a given `Sink`.
+   *
+   * @param statsSink the `Sink` that will consume throughput statistics
+   * @tparam A the type of the elements passing through this stage
+   * @tparam Mat the value materialized by `statsSink`
+   * @return a `Flow` that outputs all its inputs and emits throughput stats to `statsSink`.
+   */
   def apply[A, Mat](statsSink: Sink[Stats, Mat]): Graph[FlowShape[A, A], Mat] = {
     GraphDSL.create(statsSink) { implicit b => sink =>
       import GraphDSL.Implicits._
@@ -63,6 +97,14 @@ object ThroughputMonitor {
     }
   }
 
+  /**
+   * Creates a `ThroughputMonitor` stage with throughput stats handled periodically by a callback.
+   *
+   * @param statsInterval the update frequency of the throughput stats
+   * @param onStats the function to call when a throughput stats bucket is available
+   * @tparam A the type of the elements passing through this stage
+   * @return a `Flow` that outputs all its inputs and calls `onStats` frequently with throughput stats.
+   */
   def apply[A](statsInterval: FiniteDuration, onStats: Stats => Unit): Graph[FlowShape[A, A], NotUsed] =
     apply(Flow.fromGraph(new Pulse[Stats](statsInterval)).to(Sink.foreach(onStats)).mapMaterializedValue(_ => NotUsed))
 }
